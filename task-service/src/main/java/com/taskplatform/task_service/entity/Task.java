9646e6bbed53657da5e48c426495ba8a2c097939
zaimy nabil <zaimynabil@zaimys-MacBook-Air.local>
2026-09-03 01:14:17 +0800

    Add Task entity with TaskStatus enum and ManyToOne relationship to Project

---
+++ task-service/src/main/java/com/taskplatform/task_service/entity/Task.java
@@ -0,0 +1,52 @@
+package com.taskplatform.task_service.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table (name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+
+    @Column(name = "assignee_id")
+    private Long assigneeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id" , nullable = false)
+    private Project project;
+
+    @Column(name = "created_at" , updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @PrePersist
+    void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+        if (status == null) {
+            status = TaskStatus.TODO;
+        }
+    }
+
+    @PreUpdate
+    void onUpdate(){
+            updatedAt = Instant.now();
+        }
+
+    }
---
+++ task-service/src/main/java/com/taskplatform/task_service/entity/TaskStatus.java
@@ -0,0 +1,8 @@
+package com.taskplatform.task_service.entity;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    IN_REVIEW,
+    DONE
+}
