7352e813a548bf1c09e6eb5ffed1b4e3f0343479
zaimynabil <nabilzaimy012@gmail.com>
2026-09-06 20:26:08 +0800

    Complete TaskService with full CRUD operations

--- task-service/src/main/java/com/taskplatform/task_service/service/TaskService.java
+++ task-service/src/main/java/com/taskplatform/task_service/service/TaskService.java
@@ -6,6 +6,8 @@ import com.taskplatform.task_service.repository.ProjectRepository;
 import com.taskplatform.task_service.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
 
@@ -25,5 +27,33 @@ public class TaskService {
 
         task.setProject(project);
         return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public List<Task> getTaskByProject(Long projectId){
+        return taskRepository.findByProjectId(projectId);
+    }
+
+    public Task getTaskById(Long id){
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found with id: " + id));
+    }
+
+    public Task updateTask(Long id , Task updatedTask){
+        Task existing = getTaskById(id);
+        existing.setTitle(updatedTask.getTitle());
+        existing.setDescription(updatedTask.getDescription());
+        existing.setStatus(updatedTask.getStatus());
+        existing.setAssigneeId(updatedTask.getAssigneeId());
+        return taskRepository.save(existing);
+
+    }
+
+    public void deleteTask(Long id) {
+        Task existing = getTaskById(id);
+        taskRepository.delete(existing);
     }
 }
